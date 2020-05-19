4706a28f2afd7491ddd1ef15d8e1bd11e146c744
Gnorbi951 <gnorbi951@gmail.com>
2020-05-19 11:36:28 +0200

    Get response from my server

--- src/main/java/com/codecool/si/Main.java
+++ src/main/java/com/codecool/si/Main.java
@@ -1,7 +1,22 @@
 package com.codecool.si;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(8000), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert server != null;
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 }
---
+++ src/main/java/com/codecool/si/MyHandler.java
@@ -0,0 +1,21 @@
+package com.codecool.si;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MyHandler implements HttpHandler {
+
+    public void handle(HttpExchange t) throws IOException {
+        //InputStream is = t.getRequestBody();
+        //read(is); // .. read the request body
+        String response = "This is the response";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
